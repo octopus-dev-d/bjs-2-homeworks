77e639c309a0f2efcbefeddc3baa70e4900f14ea
Octopus <octopus.dev@yandex.kz>
2022-03-29 10:18:19 +0600

    task.js and main.js completed

--- 8.decorators/main.js
+++ 8.decorators/main.js
@@ -7,14 +7,18 @@ upgradedAddThree(3, 2, 3);
 upgradedAddThree(4, 2, 3); 
 upgradedAddThree(5, 2, 3); 
 upgradedAddThree(6, 2, 3); 
-upgradedAddThree(1, 2, 3); 
+upgradedAddThree(1, 2, 3);
+upgradedAddThree(6, 2, 5); 
 
-const sendSignal = () => console.log("Сигнал отправлен");
-const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
-setTimeout(upgradedSendSignal); // Сигнал отправлен
-setTimeout(upgradedSendSignal, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс (300 - 0 < 2000)
-setTimeout(upgradedSendSignal, 900); // проигнорировано так как времени от последнего вызова прошло: 900-300=600 (600 < 2000)
-setTimeout(upgradedSendSignal, 1200); // проигнорировано так как времени от последнего вызова прошло: 1200-900=300 (600 < 2000)
-setTimeout(upgradedSendSignal, 2300); // проигнорировано так как времени от последнего вызова прошло: 2300-1200=1100 (1100 < 2000)
-setTimeout(upgradedSendSignal, 4400); // Сигнал отправлен так как времени от последнего вызова прошло: 4400-2300=2100 (2100 > 2000)
-setTimeout(upgradedSendSignal, 4500); // Сигнал будет отправлен, так как последний вызов debounce декоратора (спустя 4500 + 2000 = 6500) 6,5с
+const sendSignal = (id) => console.log(id);
+const upgradedSendSignal = debounceDecorator2(sendSignal, 2000);
+setTimeout(() => {upgradedSendSignal(1)}); // Сигнал отправлен
+setTimeout(() => {upgradedSendSignal(2)}, 300); // проигнорировано так как от последнего вызова прошло менее 2000мс (300 - 0 < 2000)
+setTimeout(() => {upgradedSendSignal(3)}, 900); // проигнорировано так как времени от последнего вызова прошло: 900-300=600 (600 < 2000)
+setTimeout(() => {upgradedSendSignal(4)}, 1200); // проигнорировано так как времени от последнего вызова прошло: 1200-900=300 (300 < 2000)
+setTimeout(() => {upgradedSendSignal(5)}, 2300); // проигнорировано так как времени от последнего вызова прошло: 2300-1200=1100 (1100 < 2000)
+setTimeout(() => {upgradedSendSignal(6)}, 4400); // Сигнал отправлен так как времени от последнего вызова прошло: 4400-2300=2100 (2100 > 2000)
+setTimeout(() => {upgradedSendSignal(7)}, 4500);
+setTimeout(() => {upgradedSendSignal(8)}, 4700);
+
+setTimeout(() => console.log(upgradedSendSignal.count), 7000)
--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,67 @@
+// Задача 1
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+  const cache = [];
+  function cashing(...args){
+    let hash = String(args);
+    let yes = cache.findIndex((index)=> index.hash == hash);
+    if (yes != -1) {
+      let result = cache[yes].result;            
+      console.log("Из кэша: "+ result); 
+      return "Из кэша: " + result;       
+    } 
+    let result = func(...args);
+
+      if (cache.length>=5){
+        cache.shift()
+      } 
+               
+       cache.push({
+         result:result,
+         hash:hash
+        });            
+       console.log("Вычисляем: " + result); 
+       return "Вычисляем: " + result;       
+      }
+      return cashing;
 }
+
+// Задача 2
 
 
 function debounceDecoratorNew(func) {
-  // Ваш код
+  let allImmediate;
+  let flag = false;
+  return function wrapper(...args) {
+    if (!flag) {
+      func.apply(this, args);
+    }
+  flag = true;
+  clearTimeout(allImmediate);
+    allImmediate = setTimeout(() => {
+      func.apply(this, args);
+    flag = false;
+    }, ms);
+  };
 }
 
+// Задача 3
+
 function debounceDecorator2(func) {
-  // Ваш код
+  let allImmediate;
+  let flag = false;
+  wrapper.count = 0;
+  function wrapper(...args) {
+  wrapper.count ++;
+    if (!flag) {
+      func.apply(this, args);  
+    }
+  flag = true;
+	clearTimeout(allImmediate);
+    allImmediate = setTimeout(() => {
+      func.apply(this, args);
+	    flag = false;
+    }, ms);  
+  };
+  return wrapper;
 }
